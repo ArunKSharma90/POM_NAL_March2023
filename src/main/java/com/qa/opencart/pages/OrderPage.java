70edcb0765e946cf5a1318dfbff2e55136c4973b
ArunKSharma90 <arunk.sharma90@gmail.com>
2023-04-03 16:21:20 +0530

    add OrderPage file

---
+++ src/main/java/com/qa/opencart/pages/OrderPage.java
@@ -0,0 +1,12 @@
+package com.qa.opencart.pages;
+
+public class OrderPage {
+	
+	String str; 
+	
+	public void placeOrder() {
+		System.out.println("Doing it for creating a new branch in GIT");
+		
+	}
+
+}
