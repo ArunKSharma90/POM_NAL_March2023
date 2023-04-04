6232f4fdf937913b4ab66e47233a2d972ef7bdfc
ArunKSharma90 <arunk.sharma90@gmail.com>
2023-04-04 15:20:11 +0530

    login page updated

--- src/main/java/com/qa/opencart/pages/LoginPage.java
+++ src/main/java/com/qa/opencart/pages/LoginPage.java
@@ -1,11 +1,8 @@
 package com.qa.opencart.pages;
-
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-
 import com.qa.opencart.utils.Constants;
 import com.qa.opencart.utils.ElementUtil;
-
 import io.qameta.allure.Step;
 
 public class LoginPage {
@@ -17,7 +14,6 @@ public class LoginPage {
 	public LoginPage(WebDriver driver) {
 		this.driver = driver;
 		eleUtil = new ElementUtil(driver);
-		
 	}
 
 //	3. Create By locators
